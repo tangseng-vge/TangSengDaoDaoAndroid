98f4c4c284b740ee2f990b39e385c57d82cf7ca7
abc <abc@abcdeMacBook-Pro.local>
2026-07-20 18:32:38 +0800

    fix: 修正后台保活服务引用

--- wkuikit/src/main/java/com/chat/uikit/WKIMKeepAliveService.java
+++ wkuikit/src/main/java/com/chat/uikit/WKIMKeepAliveService.java
@@ -1,11 +1,11 @@
 package com.chat.uikit;
 
-import android.app.JobScheduler;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.app.Service;
+import android.app.job.JobScheduler;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ServiceInfo;
